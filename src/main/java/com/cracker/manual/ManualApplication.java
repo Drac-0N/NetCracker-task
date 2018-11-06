548db9c59f4d80f61e35588f075ceef7beacf023
Drac-0N <33728533+Drac-0N@users.noreply.github.com>
2018-11-06 15:48:31 +0400

    create

---
+++ src/main/java/com/cracker/manual/ManualApplication.java
@@ -0,0 +1,12 @@
+package com.cracker.manual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManualApplication.class, args);
+    }
+}
