22ca438664c054f7c8b4c4bf03887ebaf0a4cb9d
Drac-0N <33728533+Drac-0N@users.noreply.github.com>
2018-11-16 17:13:52 +0400

    task1

---
+++ src/main/java/model/Group.java
@@ -0,0 +1,17 @@
+package model;
+import lombok.Data;
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Data
+@Entity
+@Table(name = "student")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String name;
+    private String faculty;
+    private ArrayList<Student> students;
+}
